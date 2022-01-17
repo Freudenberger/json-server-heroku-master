fff4ef734265112d1e3d0ce3d395d6cbb96a586c
kk <kkijas@jaktestowac.pl>
2022-01-17 13:31:36 +0100

    Added posts page

--- public/demos/all/js/posts.js
+++ public/demos/all/js/posts.js
@@ -1,16 +1,39 @@
 const endpoint = '../../posts';
+const endpoints = [
+  '../../posts',
+  '../../users'
+];
+
+//const issueGetRequest = () => {
+//    // get data from the server:
+//    console.log('GET request:', endpoint);
+//    fetch(endpoint)
+//        .then(response => response.json())
+//        .then(displayPostsData);
+//};
+
 const issueGetRequest = () => {
     // get data from the server:
-    console.log('GET request:', endpoint);
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(displayPostsData);
+    console.log('GET request:', endpoints);
+    const results = await Promise.all(endpoints.map((url) => fetch(url).then((r) => r.json())));
+    const postsData = results[0]
+    const usersData = results[1]
+
+    for (const i = 0; i < postsData.length; i++) {
+        for (const j = 0; j < usersData.length; j++) {
+            if (usersData[j].id === postsData[i].user_id) {
+                postsData[i] = `${usersData[j].firstname} ${usersData[j].lastname}`
+            }
+        }
+    }
+
+    displayPostsData(postsData)
 };
 
 const getItemHTML = (item) => {
     return `<div>
         <label>id:</label><span>${item.id}</span><br>
-        <label>user_id:</label><span>${item.user_id}</span><br>
+        <label>user name:</label><span>${item.user_name}</span><br>
         <label>date:</label><span>${item.date}</span><br>
         <label>body:</label><span>${item.body}</span><br>
     </div>`;
