89e1c26a59bd48d8533b6866bb82551ed52b779f
kk <kkijas@jaktestowac.pl>
2022-01-17 12:44:14 +0100

    Added posts page

--- public/demos/all/js/posts.js
+++ public/demos/all/js/posts.js
@@ -10,26 +10,13 @@ const issueGetRequest = () => {
 
 const getItemHTML = (item) => {
     return `<div>
-//        <div class="controls">
-//            <i class="fas fa-edit edit" id="${item.id}"></i>
-//            <i class="fas fa-trash delete" id="${item.id}"></i>
-//        </div>
         <label>id:</label><span>${item.id}</span><br>
         <label>user_id:</label><span>${item.user_id}</span><br>
         <label>date:</label><span>${item.date}</span><br>
         <label>body:</label><span>${item.body}</span><br>
-//        <label>avatar:</label><img src="${item.avatar}" />
     </div>`;
 };
 
-const displayItem = (item, container) => {
-    itemHTML = getItemHTML(item);
-    container.innerHTML += `
-        <div class="card-wrapper">${itemHTML}</div>
-    `;
-};
-
-// posts:
 const displayPostsData = (data) => {
     console.log(data)
     const container = document.querySelector("#postContainer");
@@ -37,6 +24,13 @@ const displayPostsData = (data) => {
     for (item of data) {
         displayItem(item, container);
     }
+};
+
+const displayItem = (item, container) => {
+    itemHTML = getItemHTML(item);
+    container.innerHTML += `
+        <div class="card-wrapper">${itemHTML}</div>
+    `;
 };
 
 issueGetRequest();
