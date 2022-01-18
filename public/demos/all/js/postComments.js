03fcd8777fa6daa113a39a2e1cf14f79e5ee2d6c
kk <kkijas@jaktestowac.pl>
2022-01-18 19:25:04 +0100

    Major changes

--- public/demos/all/js/postComments.js
+++ public/demos/all/js/postComments.js
@@ -31,6 +31,7 @@ async function issueGetRequest(post_id)
     postData.comments.sort((a,b) => a.date < b.date);
 
     displayPostsData(postsData);
+    attachEventHandlers();
 };
 
 const getImagesHTML = (images) => {
@@ -44,8 +45,12 @@ const getImagesHTML = (images) => {
     return htmlData
 };
 
+//            <i class="fas fa-trash delete" id="${item.id}"></i>
 const getItemHTML = (item) => {
     return `<div>
+        <div class="controls">
+            <i class="fas fa-edit edit" id="${item.id}"></i>
+        </div>
         <label>title:</label><span>${item.title}</span><br>
         <label>id:</label><span>${item.id}</span><br>
         <label>user name:</label><span>${item.user_name}</span><br>
@@ -113,3 +118,144 @@ function getParams()
 
 const post_id = getParams()['id']
 issueGetRequest(post_id);
+
+
+
+
+
+
+const issuePutRequest = (id, data, responseHandler) => {
+    // update data on the server:
+    const url = postsEndpoint + '/' + id;
+    console.log('PUT request:', url, data);
+    fetch(url, {
+            method: 'put',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(responseHandler);
+};
+
+const issueDeleteRequest = (id, responseHandler) => {
+    // delete data on the server:
+    const url = postsEndpoint + '/' + id;
+    console.log('DELETE request:', url);
+    fetch(url, { method: 'delete' })
+        .then(responseHandler);
+};
+
+const issuePostRequest = (data, responseHandler) => {
+   // create data on the server:
+   console.log('POST request:', postsEndpoint, data);
+   fetch(postsEndpoint, {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(responseHandler);
+};
+
+const handleUpdate = (ev) => {
+    const id = ev.target.getAttribute('data-id');
+    const container = ev.target.parentElement.parentElement;
+    const data = {
+        'title': container.querySelector('#title').value,
+        'body': container.querySelector('#body').value,
+        'user_id': container.querySelector('#user_id').value,
+        'date': container.querySelector('#date').value,
+    };
+    const callback = (item) => {
+        container.innerHTML = getItemHTML(item);
+        attachEventHandlers();
+    };
+    issuePutRequest(id, data, callback)
+};
+
+const handleCreate = () => {
+    const container = document.querySelector('.add-new-panel');
+    data = {
+        'title': container.querySelector('#title').value,
+        'body': container.querySelector('#body').value,
+    }
+    issuePostRequest(data, issueGetRequest);
+    document.querySelector('.add-new-panel').classList.remove('active');
+};
+
+const handleDelete = (ev) => {
+    const id = ev.target.id;
+    const areYouSure = confirm('Are you sure that you want to delete item #' + id + '?')
+    if (!areYouSure) {
+        return;
+    }
+    issueDeleteRequest(id, issueGetRequest);
+};
+
+const attachEventHandlers = () => {
+    for (elem of document.querySelectorAll('.delete')) {
+        elem.onclick = handleDelete;
+    }
+    for (elem of document.querySelectorAll('.edit')) {
+        elem.onclick = showEditForm;
+    }
+//    document.querySelector('#add-new').onclick = () => {
+//        const container = document.querySelector('.add-new-panel');
+//        container.querySelector('.firstname').value = '';
+//        container.querySelector('.firstname').value = '';
+//        container.classList.add('active');
+//    };
+    document.querySelector('.close').onclick = () => {
+        document.querySelector('.add-new-panel').classList.remove('active');
+    };
+    document.querySelector('.add-new-panel .cancel').onclick = () => {
+        document.querySelector('.add-new-panel').classList.remove('active');
+    };
+    document.querySelector('.update.save').onclick = handleCreate;
+
+};
+
+const attachFormEventHandlers = (item, container) => {
+    container.querySelector('.update').onclick = handleUpdate;
+    container.querySelector('.cancel').onclick = () => {
+        container.innerHTML = getItemHTML(item);
+        attachEventHandlers();
+    }
+};
+
+const showEditForm = (ev) => {
+    const id = ev.target.id;
+    const url = postsEndpoint + '/' + id;
+    const cardElement = ev.target.parentElement.parentElement;
+    fetch(url)
+        .then(response => response.json())
+        .then(item => {
+            displayForm(item, cardElement);
+            attachFormEventHandlers(item, cardElement);
+        });
+    return false;
+};
+
+const displayForm = (item, container) => {
+    container.innerHTML = `
+        <div style="margin-top:7px; ">
+            <label>id:</label><span>${item.id}</span><br>
+            <label>title:</label>
+            <input type="text" id="title" value="${item.title}"><br>
+            </br>
+            <label>body:</label><br>
+            <textarea rows="4" type="text" id="body" value="${item.body}">${item.body}</textarea><br>
+            <input style="visibility:hidden;" type="text" id="user_id" value="${item.user_id}"><br>
+            <input style="visibility:hidden;" type="text" id="date" value="${item.date}"><br>
+
+    <div align="center" >
+            <label></label><br>
+            <button type="button" data-id="${item.id}" class="update button-primary">Update</button>
+            <button type="button" class="cancel">Cancel</button>
+        </div></div>
+    `;
+};
--- public/demos/all/js/posts.js
+++ public/demos/all/js/posts.js
@@ -1,21 +1,19 @@
-const endpoint = '../../posts';
-const endpoints = [
-  '../../posts',
-  '../../users'
-];
+const postsEndpoint = '../../posts'
+const usersEndpoint = '../../users'
+
 
 async function issueGetRequest()
 {
     // get data from the server:
-    console.log('GET request:', endpoints);
-    const results = await Promise.all(endpoints.map((url) => fetch(url).then((r) => r.json())));
+    const results = await Promise.all([postsEndpoint, usersEndpoint].map((url) => fetch(url).then((r) => r.json())));
     const postsData = results[0]
     const usersData = results[1]
 
     for (let i = 0; i < postsData.length; i++) {
         for (let j = 0; j < usersData.length; j++) {
-            if (usersData[j].id === postsData[i].user_id) {
+            if (usersData[j].id?.toString() === postsData[i].user_id?.toString()) {
                 postsData[i].user_name = `${usersData[j].firstname} ${usersData[j].lastname}`;
+                postsData[i].user_id = usersData[j].id;
                 break;
             }
         }
@@ -29,10 +27,12 @@ async function issueGetRequest()
 
 const getImagesHTML = (images) => {
     let htmlData = "";
+    if (images !== undefined) {
         for (image of images) {
             htmlData += `<img src="${image}" />`;
             htmlData += `<br>`
         }
+    }
     return htmlData
 };
 
@@ -40,7 +40,7 @@ const getImagesHTML = (images) => {
 //        <label>id:</label><span>${item.id}</span><br>
 const getItemHTML = (item) => {
     return `<div>
-        <label>user name:</label><span>${item.user_name}</span><br>
+        <label>user name:</label><span><a href="user.html?id=${item.user_id}">${item.user_name}</a></span><br>
         <label>date:</label><span>${item.date}</span><br>
         <label>title:</label><span>${item.title}</span><br>
         ${getImagesHTML(item.images)}
