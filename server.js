11362d8929ed375869bcbce9836791d16ea68f9b
kk <kkijas@jaktestowac.pl>
2022-01-28 17:09:55 +0100

    Added iframe for swagger

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const jsonServer = require('./json-server');
 const fs = require('fs');
 const path = require('path');
 
+let updatedSchema = false;
 
 const customRoutes = (req, res, next) => {
   try {
+    if (!updatedSchema) {
+      console.log(req.headers)
+      const host = req.headers.host;
+      const referer = req.headers.referer;
+      const schema = JSON.parse(fs.readFileSync(path.join(__dirname, 'public/tools/schema/openapi_rest_demo.json'), 'utf8'));
+      schema['servers'][0]['url'] = `${referer.split(':')[0]}://${host}/api`;
+      fs.writeFileSync(path.join(__dirname, 'public/tools/schema/openapi_rest_demo.json'), JSON.stringify(schema));
+      updatedSchema = true;
+    }
     if (req.method === 'GET' && req.url.endsWith('/reloadDB')) {
       const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db-base.json'), 'utf8'));
       router.db.setState(db);
