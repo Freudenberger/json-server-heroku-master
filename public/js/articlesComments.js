3bd3c2a355d1c045a78fe16a3d4478560dfc9e17
kk <kkijas@jaktestowac.pl>
2022-01-29 16:58:19 +0100

    Added improvements to comments

--- public/js/articlesComments.js
+++ public/js/articlesComments.js
@@ -9,11 +9,16 @@ const endpoints = [
 async function issueGetRequest()
 {
     // get data from the server:
-    console.log('GET request:', endpoints);
     const results = await Promise.all(endpoints.map((url) => fetch(url).then((r) => r.json())));
     const articlesData = results[0]
     const usersData = results[1]
     const userComments = results[2]
+    const tempUserData = {}
+
+    for (let j = 0; j < usersData.length; j++) {
+        const user_name = `${usersData[j].firstname} ${usersData[j].lastname}`
+        tempUserData[usersData[j].id.toString()] = user_name;
+    }
 
     for (let i = 0; i < articlesData.length; i++) {
         for (let j = 0; j < usersData.length; j++) {
@@ -28,6 +33,7 @@ async function issueGetRequest()
         articlesData[i].comments = []
         for (let j = 0; j < userComments.length; j++) {
             if (userComments[j].article_id === articlesData[i].id) {
+                userComments[j].user_name = tempUserData[userComments[j].user_id.toString()]
                 articlesData[i].comments.push(userComments[j]);
             }
         }
@@ -38,15 +44,18 @@ async function issueGetRequest()
     displayArticlesData(articlesData);
 };
 
+{/* <label>Article id:</label><span>${item.id}</span><br> */}
 const getItemHTML = (item) => {
     return `<div>
-        <label>id:</label><span>${item.id}</span><br>
-        <label>user:</label><span>${item.user_name}</span><br>
+        <label>user:</label><span><a href="user.html?id=${item.user_id}">${item.user_name}</a></span><br>
         <label>date:</label><span>${item.date}</span><br>
-        <label>title:</label><span>${item.title}</span><br>
-        <label>description:</label><span>${item.body}</span><br>
-        <hr><br>
-        <label>comments:</label><br>
+        <span><strong>${item.title}</strong></span><br>
+        <span>${item.body?.substring(0, 200)}</span><br>
+        <span><a href="article.html?id=${item.id}">See whole article...</a></span><br>
+        <br>
+        <hr>
+        <h3>Comments:</h3>
+        <hr>
         ${getCommentsHTML(item.comments)}
     </div>`;
 };
@@ -67,11 +76,13 @@ const getCommentsHTML = (comments) => {
     return htmlData
 };
 
-const getCommentHTML = (comments) => {
+{/* <label>id:</label><span>${comments.id}</span><br> */}
+const getCommentHTML = (comment) => {
     return `<div>
-        <label>id:</label><span>${comments.id}</span><br>
-        <label>date:</label><span>${comments.date}</span><br>
-        <label>comment:</label><span>${comments.body}</span><br>
+        <label>user:</label><span><a href="user.html?id=${comment.user_id}">${comment.user_name}</a></span><br>
+        <label>date:</label><span>${comment.date}</span><br>
+        <label>comment:</label><span>${comment.body}</span><br>
+        <span><a href="comment.html?id=${comment.id}">See comment...</a></span><br>
     </div>`;
 };
 
