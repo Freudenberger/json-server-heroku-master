616d06019235607b32265958f1996f20771b9508
kk <kkijas@jaktestowac.pl>
2022-04-11 23:38:54 +0200

    Added sample jwt auth

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const jsonServer = require("./json-server");
 const { validations } = require("./validators");
 const fs = require("fs");
+const jwt = require('jsonwebtoken')
+const authUserDb = './auth-users.json'
+const userdb = JSON.parse(fs.readFileSync(authUserDb, 'UTF-8'))
+
 
 let updatedSchema = false;
 
@@ -81,9 +85,137 @@ const customRoutes = (req, res, next) => {
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+
+
+// JWT: based on https://github.com/techiediaries/fake-api-jwt-json-server/blob/master/server.js
+const SECRET_KEY = "123456789";
+
+const expiresIn = "1h";
+
+// Create a token from a payload
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+
+// Verify the token
+function verifyToken(token) {
+  return jwt.verify(token, SECRET_KEY, (err, decode) =>
+    decode !== undefined ? decode : err
+  );
+}
+
+// Check if the user exists in database
+function isAuthenticated({ email, password }) {
+  return (
+    userdb.users.findIndex(
+      (user) => user.email === email && user.password === password
+    ) !== -1
+  );
+}
+
+// Register New User
+server.post("/auth/register", (req, res) => {
+  logDebug("register endpoint called; request body:");
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  if (isAuthenticated({ email, password }) === true) {
+    const status = 401;
+    const message = "Email and Password already exist";
+    res.status(status).json({ status, message });
+    return;
+  }
+
+  fs.readFile(authUserDb, (err, data) => {
+    if (err) {
+      const status = 401;
+      const message = err;
+      res.status(status).json({ status, message });
+      return;
+    }
+
+    // Get current users data
+    var data = JSON.parse(data.toString());
+
+    // Get the id of last user
+    var last_item_id = data.users[data.users.length - 1].id;
+
+    //Add new user
+    data.users.push({ id: last_item_id + 1, email: email, password: password }); //add some data
+    var writeData = fs.writeFile(
+      authUserDb,
+      JSON.stringify(data),
+      (err, result) => {
+        // WRITE
+        if (err) {
+          const status = 401;
+          const message = err;
+          res.status(status).json({ status, message });
+          return;
+        }
+      }
+    );
+  });
+
+  // Create token for new user
+  const access_token = createToken({ email, password });
+  logDebug("Access Token:" + access_token);
+  res.status(200).json({ access_token });
+});
+
+// Login to one of the users from ./users.json
+server.post("/auth/login", (req, res) => {
+  logDebug("login endpoint called; request body:");
+  console.log(req.body);
+  const { email, password } = req.body;
+  if (isAuthenticated({ email, password }) === false) {
+    const status = 401;
+    const message = "Incorrect email or password";
+    res.status(status).json({ status, message });
+    return;
+  }
+  const access_token = createToken({ email, password });
+  logDebug("Access Token:" + access_token);
+  res.status(200).json({ access_token });
+});
+
+// server.use(/^(?!\/auth).*$/, (req, res, next) => {
+server.use(/(\/auth\/api).*$/, (req, res, next) => {
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(" ")[0] !== "Bearer"
+  ) {
+    const status = 401;
+    const message = "Error in authorization format";
+    res.status(status).json({ status, message });
+    return;
+  }
+  try {
+    let verifyTokenResult;
+    logDebug("req.headers.authorization:" + req.headers.authorization);
+    const access_token = req.headers.authorization.split(" ")[1]
+    logDebug("Access Token:" + access_token);
+    verifyTokenResult = verifyToken(access_token);
+
+    if (verifyTokenResult instanceof Error) {
+      const status = 401;
+      const message = "Access token not provided";
+      res.status(status).json({ status, message });
+      return;
+    }
+    next();
+  } catch (err) {
+    const status = 401;
+    const message = "Error access_token is revoked";
+    res.status(status).json({ status, message });
+  }
+});
+
+
 server.use(customRoutes);
 server.use(validations);
 server.use("/api", router);
+server.use("/auth/api", router);
 
 server.listen(port, () => {
   logDebug(`Test Custom Data API listening on port ${port}!`);
